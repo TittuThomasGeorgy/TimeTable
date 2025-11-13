da3a582e84d1d2572dd1768177d1c27a9fea0cec
TittuThomasGeorgy <117733012+TittuThomasGeorgy@users.noreply.github.com>
2025-11-13 23:34:22 +0530

    Update HomePage.tsx

--- client/src/modules/home/pages/HomePage.tsx
+++ client/src/modules/home/pages/HomePage.tsx
@@ -8,7 +8,7 @@ const HomePage = () => {
         <>
             <CommonPageLayout>
                 
-                <div>HomePage</div>
+                
                 <div>HomePage</div>
                 <div>HomePage</div>
                 <div>HomePage</div>
